953d6eb557b4dee2ec70d402ea6da0c8e68ef31a
=Christian Titus Fernanda <christiantitus.2020@student.uny.ac.id>
2022-10-27 09:43:26 +0700

    php_dasar1

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+$NamaSaya = "Titus" ;
+$UmurSaya = "20 tahun" ;
+$AsalSaya = "Kabupaten Sleman" ;
+
+    echo "Halo dunia! Nama saya <strong>$NamaSaya</strong>, usia <strong>$UsiaSaya</strong>, asal dari <strong>$AsalSaya</strong>.";
+
+?>
